526bb2ee5e0c65b9ece02f362e91b76759a422fc
alfredobs97 <alfredobautista1@gmail.com>
2019-12-07 14:36:09 +0100

    gradient

--- lib/gift.dart
+++ lib/gift.dart
@@ -10,9 +10,23 @@ class Gift extends StatelessWidget {
           height: MediaQuery.of(context).size.height / 4,
           width: MediaQuery.of(context).size.width,
           decoration: BoxDecoration(
-            image: DecorationImage(image: NetworkImage('https://elpais.com/tecnologia/imagenes/2017/09/19/actualidad/1505815835_481570_1505909612_noticia_fotograma.jpg'), fit: BoxFit.fill),
+            image: DecorationImage(
+                image: NetworkImage(
+                    'https://elpais.com/tecnologia/imagenes/2017/09/19/actualidad/1505815835_481570_1505909612_noticia_fotograma.jpg'),
+                fit: BoxFit.fill),
           ),
         ),
+        Container(
+          height: MediaQuery.of(context).size.height / 4,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0.4, 1],
+                colors: [Theme.of(context).backgroundColor.withOpacity(0.0), Theme.of(context).primaryColor]),
+          ),
+        )
       ]),
     );
   }
