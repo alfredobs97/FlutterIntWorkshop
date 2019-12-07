fd011a91bd5893334097c5c39efc41874447b97f
alfredobs97 <alfredobautista1@gmail.com>
2019-12-07 14:46:48 +0100

    theme

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Mi lista de regalo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.blue[200],
+        accentColor: Colors.red[400],
+        backgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+            color: Colors.white,
+            elevation: 0,
+            textTheme: TextTheme(title: TextStyle(color: Colors.indigo[200], fontSize: 25)),
+            iconTheme: IconThemeData(color: Colors.indigo[200])),
+        iconTheme: IconThemeData(color: Colors.indigo[200], size: 40),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.red[400],
+        ),
+        textTheme: TextTheme(body1: TextStyle(color: Colors.blueGrey)),
       ),
       home: MyHomePage(),
     );
