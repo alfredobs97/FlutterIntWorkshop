9fc0656c3dbc2b5188e314a7dcb3f8553f2da60a
alfredobs97 <alfredobautista1@gmail.com>
2019-12-08 01:36:30 +0100

    api dependencies and api function

--- lib/shop.dart
+++ lib/shop.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 import 'mini-gift.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Shop extends StatefulWidget {
   final Function addGift;
   final Function removeGift;
+  List<MiniGift> myGifts = List();
   Shop({this.addGift, this.removeGift});
   @override
   _ShopState createState() => _ShopState();
 }
 
 class _ShopState extends State<Shop> {
+  Future<List<MiniGift>> getGifts() async {
+    final responseJson = await http.get('http://vps675002.ovh.net:81/getGifts').then((response) => jsonDecode(response.body));
+    List<MiniGift> gifts = List();
+    responseJson.forEach((gift) {
+      gifts.add(MiniGift(
+        img: gift['img'],
+        nameGift: gift['name'],
+        addGift: widget.addGift,
+        removeGift: widget.removeGift,
+      ));
+    });
+
+    return gifts;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
